beecf7a830ffa74e60d500759bdb5e8b636a6eb6
Ondrej Ledvinka <stathydro.cz@gmail.com>
2025-05-14 14:10:36 +0200

    new project created due to an error with GitHub

---
+++ scripts/notes_from_course.R
@@ -0,0 +1,11 @@
+usethis::use_git()
+
+# alternativně lze vytvořit Git kontrolu hned při zakládání nového projketu
+
+# přidávám komentář kvůli demonstraci historie souboru
+
+# přidávám ještě jeden řádek, abych viděl, jak funguje ta historie:-)
+
+usethis::use_github()
+
+# přidávám komentář, aby s to projevilo v historii na GitHubu xxxxx
